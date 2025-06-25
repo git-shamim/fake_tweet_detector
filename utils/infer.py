164db0ce372ad9d930bca253711b78fe48313cf0
“shamim-github” <shamim.ahmed2017@icloud.com>
2025-06-25 08:54:25 +0530

    Switch back to pretrained model from Hugging Face

--- utils/infer.py
+++ utils/infer.py
@@ -4,8 +4,6 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from utils.preprocess import clean_tweet
 from utils.config import USE_FINETUNED_MODEL, MODEL_PATH
 
-LABEL_MAP = {0: "🟥 Fake", 1: "🟩 Real"}
-
 @st.cache_resource
 def load_model():
     if USE_FINETUNED_MODEL:
@@ -14,10 +12,8 @@ def load_model():
     else:
         tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base")
         model = AutoModelForSequenceClassification.from_pretrained("vinai/bertweet-base", num_labels=2)
-
     model.eval()
     return tokenizer, model
-
 
 tokenizer, model = load_model()
 
@@ -27,5 +23,15 @@ def predict_tweet(text):
     with torch.no_grad():
         outputs = model(**inputs)
         logits = outputs.logits
-        pred = torch.argmax(logits, dim=1).item()
-    return LABEL_MAP[pred], torch.softmax(logits, dim=1).tolist()[0]
+        probs = torch.softmax(logits, dim=1).tolist()[0]
+        fake_prob, real_prob = probs
+
+    # Interpret result based on real class probability
+    if real_prob > 0.60:
+        label = "🟩 Very likely genuine"
+    elif real_prob < 0.40:
+        label = "🟥 Very likely fake"
+    else:
+        label = "🟧 Uncertain or unclear"
+
+    return label, probs
